22fc38b9504da538696195302c47edccad6ee1a1
Vishal Pandit <152159518+TrooperDG@users.noreply.github.com>
2025-07-26 13:56:32 +0530

    updated user model and user route

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -9,10 +9,14 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
       required: true,
+    },
+    passcode: {
+      type: String,
     },
   },
   { timestamps: true }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,13 +3,21 @@ import {
   login,
   register,
   voterAccess,
+  toggleVote,
 } from "../controllers/user.controller.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
+import { hasVoterAccess } from "../middlewares/voterAuth.middleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/login", login);
 userRouter.post("/register", register);
 userRouter.post("/voter-access", isAuthenticated, voterAccess);
+userRouter.patch(
+  "/toggle-vote/:id",
+  isAuthenticated,
+  hasVoterAccess,
+  toggleVote
+);
 
 export default userRouter;
